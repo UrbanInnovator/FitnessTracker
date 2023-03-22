e9bd377e35b793f8476fad2c015e09e914460780
Tippy <Pixie2shot@gmail.com>
2023-03-22 10:06:40 -0500

    all databases created and all drop tables created

--- db/seedData.js
+++ db/seedData.js
@@ -9,7 +9,10 @@ async function dropTables() {
     console.log("Dropping All Tables...")
 
     await client.query(`
-    DROP TABLE IF EXIST users
+    DROP TABLE IF EXIST users;
+    DROP TABLE IF EXIST activities;
+    DROP TABLE IF EXIST routines;
+    DROP TABLE IF EXIST routine_activities;
     `);
   }catch(err) {
     console.log(err);
@@ -22,10 +25,34 @@ async function createTables() {
   console.log("Starting to build tables...")
 
   await client.query(`
-    CREATE TABLE users
-    Id SERIAL PRIMARY KEY,
+    CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
+    );
+
+    CREATE TABLE activities(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      description TEXT NOT NULL
+    );
+
+    CREATE TABLE routines(
+      id SERIAL PRIMARY KEY,
+      "creatorId" INTERGER REFERENCES users(id),
+      "isPublic" BOOLEAN DEFAULT false,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      goal TEXT NOT NULL
+    );
+
+    CREATE TABLE routine_activities(
+      id SERIAL PRIMARY KEY,
+      "routineId" INTEGER REFERENCES routines(id),
+      "activityId" INTEGER REFERENCES activities(id),
+      duration INTERGER,
+      count INTERGER, 
+      UNIQUE ("routineId", "activityId")
+    )
   `);
  }catch(err){
    console.log(err);
