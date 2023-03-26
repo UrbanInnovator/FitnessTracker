5c637a2aecf1f4587ce3f0d353519f2088b4eea1
UrbanInnovator <suburbaninventor@gmail.com>
2023-03-26 12:29:02 -0500

    seedData done

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -7,7 +7,13 @@ async function addActivityToRoutine({
   duration,
 }) {
   try {
-    const
+    const { rows: [ routineActivity ] } = await client.query(`
+      INSERT INTO routine_activities("routineId", "activityId", count, duration)
+      VALUES($1, $2, $3, $4)
+      RETURNING *;
+      `, [routineId, activityId, count, duration]);
+
+      return routineActivity;
   } catch (error) {
     console.log(error);
   }
